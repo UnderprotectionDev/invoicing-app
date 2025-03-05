71d7219f39e697668fd5b51fdd45f99ef0811395
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-05 18:29:51 +0300

    I apolog

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,50 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function Dashboard() {
+  return (
+    <main className="flex flex-col justify-center h-full text-center gap-4">
+      <h1 className="text-3xl font-bold">Invoices</h1>
+
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px] p-4">Date</TableHead>
+            <TableHead className="p-4">Customer</TableHead>
+            <TableHead className="p-4">Email</TableHead>
+            <TableHead className="text-center p-4">Status</TableHead>
+            <TableHead className="text-right p-4">Value</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">
+              <span className="font-semibold">11/11/2025</span>
+            </TableCell>
+            <TableCell className="text-left">
+              <span className="font-semibold">John Doe</span>
+            </TableCell>
+            <TableCell className="text-left p-4">
+              <span>john.doe@example.com</span>
+            </TableCell>
+            <TableCell className="text-center p-4">
+              <Badge className="rounded-full">Open</Badge>
+            </TableCell>
+            <TableCell className="text-right p-4">
+              <span className="font-semibold">$250.00</span>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </main>
+  );
+}
