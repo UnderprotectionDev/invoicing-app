559ab4ece7d9ba39cd380acb789c786e64da78c2
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-06 18:15:50 +0300

    Add form inputs to new invoice page

--- app/invoices/new/page.tsx
+++ app/invoices/new/page.tsx
@@ -1,21 +1,38 @@
-import { Button } from "@/components/ui/button";
-import { CirclePlus } from "lucide-react";
-import Link from "next/link";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function NewInvoice() {
   return (
-    <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto my-12">
+    <main className="flex flex-col justify-center h-full gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-bold">Create Invoice</h1>
-        <p>
-          <Button variant={"ghost"} className="inline-flex gap-2" asChild>
-            <Link href="invoices/new">
-              <CirclePlus className="size-4" />
-              Create Invoice
-            </Link>
-          </Button>
-        </p>
       </div>
+
+      <form className="grid gap-4 max-w-sm">
+        <div>
+          <Label className="block font-semibold text-sm mb-2">
+            Billing Name
+          </Label>
+          <Input type="text" />
+        </div>
+        <div>
+          <Label className="block font-semibold text-sm mb-2">
+            Billing Email
+          </Label>
+          <Input type="email" />
+        </div>
+        <div>
+          <Label className="block font-semibold text-sm mb-2">Value</Label>
+          <Input type="number" />
+        </div>
+        <div>
+          <Label className="block font-semibold text-sm mb-2">
+            Description
+          </Label>
+          <Textarea></Textarea>
+        </div>
+      </form>
     </main>
   );
 }
