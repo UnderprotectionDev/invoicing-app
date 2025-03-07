9e8bdb12610568105a7354c06d6d5342f442717a
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-07 14:10:54 +0300

    Refactor new invoice page with improved form submission handling

--- app/invoices/new/page.tsx
+++ app/invoices/new/page.tsx
@@ -1,19 +1,20 @@
 "use client";
 
 import { createAction } from "@/actions";
-import { Button } from "@/components/ui/button";
+import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
 
 export default function NewInvoice() {
   const [state, setState] = useState("ready");
-  async function handleOnSubmit(formData: FormData) {
+  async function handleOnSubmit(event: SyntheticEvent) {
     if (state === "pending") return;
     setState("pending");
+    const target = event.target as HTMLFormElement;
+    const formData = new FormData(target);
     await createAction(formData);
-    setState("ready");
   }
   return (
     <main className="flex flex-col justify-center h-full gap-6 max-w-5xl mx-auto my-12">
@@ -21,7 +22,11 @@ export default function NewInvoice() {
         <h1 className="text-3xl font-bold">Create Invoice</h1>
       </div>
 
-      <form action={createAction} className="grid gap-4 max-w-sm">
+      <form
+        action={createAction}
+        onSubmit={handleOnSubmit}
+        className="grid gap-4 max-w-sm"
+      >
         <div>
           <Label htmlFor="name" className="block font-semibold text-sm mb-2">
             Billing Name
@@ -50,9 +55,7 @@ export default function NewInvoice() {
           <Textarea id="description" name="description" />
         </div>
         <div>
-          <Button type="submit" className="w-full">
-            Create Invoice
-          </Button>
+          <SubmitButton />
         </div>
       </form>
     </main>
