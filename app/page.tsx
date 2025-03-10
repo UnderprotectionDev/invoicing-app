d1f4f1fa9d649bb61768df399a5e3753579d9138
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-10 12:26:14 +0300

    Add sign-in/sign-up buttons and route protection with Clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main className="max-w-5xl mx-auto">
       <h1 className="text-4xl font-bold">Invoicipedia</h1>
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </main>
   );
 }
