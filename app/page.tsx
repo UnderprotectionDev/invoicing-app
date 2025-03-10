36ba327efb52b51416b5f26070eace2210b5f5cd
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-10 13:25:38 +0300

    Add header component and simplify home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,7 @@
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <main className="max-w-5xl mx-auto">
       <h1 className="text-4xl font-bold">Invoicipedia</h1>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
     </main>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,32 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import { Container } from "./container";
+
+export function Header() {
+  return (
+    <header className="mt-8 mb-12">
+      <Container>
+        <p>Invoicipedia</p>
+        <div>
+          <div className="flex justify-between items-center gap-4">
+            <div className="flex items-center gap-4">
+              <p className="font-bold">
+                <Link href="/dashboard">Invoicipedia</Link>
+              </p>
+              <span className="text-slate-300">/</span>
+              <SignedIn></SignedIn>
+            </div>
+            <div>
+              <SignedOut>
+                <SignInButton />
+              </SignedOut>
+              <SignedIn>
+                <UserButton />
+              </SignedIn>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+}
