7f95c108177d7a8c49080496ff93a279757ffcd5
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-13 17:39:37 +0300

    Add dropdown menu for changing invoice status on invoice page

--- app/invoices/[invoiceId]/page.tsx
+++ app/invoices/[invoiceId]/page.tsx
@@ -4,6 +4,15 @@ import { Invoices } from "@/db/schema";
 import { and, eq } from "drizzle-orm";
 import { auth } from "@clerk/nextjs/server";
 import { Container } from "@/components/container";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { ChevronDown } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
 
 type InvoicePageProps = {
   params: Promise<{
@@ -35,6 +44,39 @@ export default async function InvoicePage({ params }: InvoicePageProps) {
   return (
     <main className="w-full h-full">
       <Container>
+        <div className="flex justify-between mb-8">
+          <h1 className="flex items-center gap-4 text-3xl font-semibold">
+            Invoice {invoice.id}
+            <Badge
+              className={cn(
+                "rounded-full capitalize",
+                invoice.status === "open" && "bg-blue-500",
+                invoice.status === "paid" && "bg-green-600",
+                invoice.status === "void" && "bg-zinc-700",
+                invoice.status === "uncollectible" && "bg-red-600"
+              )}
+            >
+              {invoice.status}
+            </Badge>
+          </h1>
+
+          <div className="flex gap-4">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  className="flex items-center gap-2"
+                  variant="outline"
+                  type="button"
+                >
+                  Change Status
+                  <ChevronDown className="w-4 h-auto" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent></DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+
         <p className="text-3xl mb-3">${(invoice.value / 100).toFixed(2)}</p>
 
         <p className="text-lg mb-8">{invoice.description}</p>
