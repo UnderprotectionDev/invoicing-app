285dc66f2262f7f1d4406876d7e15e14c9f18878
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-16 15:33:49 +0300

    Refactor dashboard data retrieval to include organizationId from authentication, allowing invoices to be filtered by organization or user, enhancing data management in the dashboard.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,20 +13,31 @@ import {
 import { db } from "@/db";
 import { Customers, Invoices } from "@/db/schema";
 import { auth } from "@clerk/nextjs/server";
-import { eq } from "drizzle-orm";
+import { and, eq, isNull } from "drizzle-orm";
 import { CirclePlus } from "lucide-react";
 import Link from "next/link";
 
 export default async function Dashboard() {
-  const { userId } = await auth();
+  const { userId, orgId } = await auth();
   if (!userId) {
     return;
   }
-  const results = await db
-    .select()
-    .from(Invoices)
-    .innerJoin(Customers, eq(Invoices.customerId, Customers.id))
-    .where(eq(Invoices.userId, userId));
+
+  let results;
+
+  if (orgId) {
+    results = await db
+      .select()
+      .from(Invoices)
+      .innerJoin(Customers, eq(Invoices.customerId, Customers.id))
+      .where(eq(Invoices.organizationId, orgId));
+  } else {
+    results = await db
+      .select()
+      .from(Invoices)
+      .innerJoin(Customers, eq(Invoices.customerId, Customers.id))
+      .where(and(eq(Invoices.userId, userId), isNull(Invoices.organizationId)));
+  }
 
   const invoices = results.map(({ invoices, customers }) => ({
     ...invoices,
