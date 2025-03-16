234a80878b3afcfbbac002f80acafb81a594b776
Mert Can <107743711+Underprotection-TR@users.noreply.github.com>
2025-03-16 15:46:31 +0300

    Refactor updateStatusAction and deleteInvoiceAction to utilize organizationId from authentication, allowing invoice updates and deletions based on organization context or user context when organizationId is null. This enhances data management for invoices.

--- actions.ts
+++ actions.ts
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
 import { db } from "./db";
 import { Customers, Invoices, Status } from "./db/schema";
-import { and, eq } from "drizzle-orm";
+import { and, eq, isNull } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 
 export async function createAction(formData: FormData) {
@@ -47,34 +47,66 @@ export async function createAction(formData: FormData) {
 }
 
 export async function updateStatusAction(formData: FormData) {
-  const { userId } = await auth();
+  const { userId, orgId } = await auth();
   if (!userId) {
     throw new Error("Unauthorized");
   }
   const id = formData.get("id") as string;
   const status = formData.get("status") as Status;
 
-  const result = await db
-    .update(Invoices)
-    .set({ status })
-    .where(and(eq(Invoices.id, parseInt(id)), eq(Invoices.userId, userId)))
-    .returning({
-      id: Invoices.id,
-    });
+  if (orgId) {
+    await db
+      .update(Invoices)
+      .set({ status })
+      .where(
+        and(
+          eq(Invoices.id, Number.parseInt(id)),
+          eq(Invoices.organizationId, orgId)
+        )
+      );
+  } else {
+    await db
+      .update(Invoices)
+      .set({ status })
+      .where(
+        and(
+          eq(Invoices.id, Number.parseInt(id)),
+          eq(Invoices.userId, userId),
+          isNull(Invoices.organizationId)
+        )
+      );
+  }
 
   revalidatePath(`/invoices/${id}`, "page");
 }
 
 export async function deleteInvoiceAction(formData: FormData) {
-  const { userId } = await auth();
+  const { userId, orgId } = await auth();
   if (!userId) {
     throw new Error("Unauthorized");
   }
   const id = formData.get("id") as string;
 
-  await db
-    .delete(Invoices)
-    .where(and(eq(Invoices.id, parseInt(id)), eq(Invoices.userId, userId)));
+  if (orgId) {
+    await db
+      .delete(Invoices)
+      .where(
+        and(
+          eq(Invoices.id, Number.parseInt(id)),
+          eq(Invoices.organizationId, orgId)
+        )
+      );
+  } else {
+    await db
+      .delete(Invoices)
+      .where(
+        and(
+          eq(Invoices.id, Number.parseInt(id)),
+          eq(Invoices.userId, userId),
+          isNull(Invoices.organizationId)
+        )
+      );
+  }
 
   redirect("/dashboard");
 }
